e302db62e6254b93eae3eb5f9f82bf57dd518507
felipe.reis@uniriotec.br <felipe.reis@uniriotec.br@169ede93-cfc7-a32f-6d2c-8c41bd67045f>
2014-10-17 03:58:26 +0000

    Implementação da interface gráfica. Exclusão da interface em texto, que estava incompleta. Inclusão do comentário // TODO nas funções da classe ServicosProduto que ainda não estão escritas.

--- trunk/TrabalhoPM/src/negocio/TrabalhoPM.java
+++ trunk/TrabalhoPM/src/negocio/TrabalhoPM.java
@@ -14,40 +14,12 @@ import java.util.Scanner;
  */
 public class TrabalhoPM {
     
-    private static final String NOME_ARQUIVO_PRODUTOS = "produtos.txt";
-    private static final String NOME_ARQUIVO_PRECOS = "precos.txt";
-
-    /**
+     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int opcao;
-        escreverMenu();
-        opcao = lerOpcao();
-        chamarOpcaoEscolhida(opcao);
+        // TODO
     }
 
-    private static void escreverMenu() {
-        System.out.println("MENU");
-        System.out.println("1 - Importar produtos");
-        System.out.println("2 - Importar precos");
-        System.out.println("Digite sua opcao: ");
-    }
-    
-    private static int lerOpcao(){
-        Scanner scanner = new Scanner(System.in);
-        int opcao = scanner.nextInt();
-        return opcao;        
-    }
-    
-    private static void chamarOpcaoEscolhida(int opcao){
-        ServicosProduto servicosProduto = new ServicosProduto();
-        if (opcao == 1)
-            servicosProduto.importarProdutosDoArquivo(NOME_ARQUIVO_PRODUTOS);
-        else if (opcao == 2)
-            servicosProduto.importarPrecosDoArquivo(NOME_ARQUIVO_PRECOS);
-        else
-            System.out.println("Opcao invalida.");
-    }
     
 }
