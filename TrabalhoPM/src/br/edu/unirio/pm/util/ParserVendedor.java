c1649d4428900d405e265832cd1c26f5387ea5ae
marcelo.castro@uniriotec.br <marcelo.castro@uniriotec.br@169ede93-cfc7-a32f-6d2c-8c41bd67045f>
2014-11-21 22:58:06 +0000

    Ajuste no ParserVendedor e em ParserVendedorTest

--- TrabalhoPM/src/br/edu/unirio/pm/util/ParserVendedor.java
+++ TrabalhoPM/src/br/edu/unirio/pm/util/ParserVendedor.java
@@ -13,24 +13,40 @@ import br.edu.unirio.pm.model.Vendedor;
  */
 public class ParserVendedor implements Parser<Vendedor> {
 
+    private long codigo;
+    private String nome;
+    int categoria;
+
     @Override
     public Vendedor processa(String linha) {
-           try {
+        try {
             String[] partes = linha.split(";");
-          //  long codigo = Long.parseLong(partes[0], 8); // O 8 é indica o sistema numérico em que a string será convertida, neste caso OCTAL. O DEFAULT é DECIMAL.
-            long codigo = Long.parseLong(partes[0]);
-            String nome = partes[1];
-            int categoria =  Integer.parseInt(partes[2]);
-            if(validaVendedor(codigo, categoria)){
-                return new Vendedor(codigo, nome, categoria);
-            }            
-        } catch (IllegalArgumentException e) {
-               System.out.println("Erro ao processar linha de Pendedor ");
+            //  long codigo = Long.parseLong(partes[0], 8); // O 8 é indica o sistema numérico em que a string será convertida, neste caso OCTAL. O DEFAULT é DECIMAL.
+            codigo = Long.parseLong(partes[0]);
+            nome = partes[1];
+            categoria = Integer.parseInt(partes[2]);
+            validaVendedor(codigo, categoria);
+            return new Vendedor(codigo, nome, categoria);
+        } catch (Exception e) {
+            System.out.println("Erro ao processar linha de Vendedor! " + e.getMessage());
         }
         return null;
     }
 
-    private boolean validaVendedor(long codigo, int categoria) {
-        return codigo > 0 && (categoria == 1 || categoria == 2);
+    private void validaVendedor(long codigo, int categoria) {
+        validaCodigo(codigo);
+        validaCategoria(categoria);
+    }
+
+    private void validaCodigo(long codigo) {
+        if (codigo <= 0) {
+            throw new IllegalArgumentException("Código inválido");
+        }
+    }
+
+    private void validaCategoria(int categoria) {
+        if (categoria != 1 && categoria != 2) {
+            throw new IllegalArgumentException("Categoria inválida");
+        }
     }
 }
--- TrabalhoPM/test/br/edu/unirio/pm/util/ParserVendedorTest.java
+++ TrabalhoPM/test/br/edu/unirio/pm/util/ParserVendedorTest.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.edu.unirio.pm.util;
 
 import br.edu.unirio.pm.model.Vendedor;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -16,36 +16,57 @@ import org.junit.Test;
  * @author MCE
  */
 public class ParserVendedorTest {
+    private Parser<Vendedor> parserVendedor;
     
-  @Test
-  public void dadoUmaStringComUmCodigoUmNomeEUmaCategoriaDeVendedorRetornaUmVendedor(){
-      Parser<Vendedor> parserVendedor = new ParserVendedor();
-      Vendedor vendedor = parserVendedor.processa("0021;JOANA ANGELICA;2");
-      
-      assertEquals(21, vendedor.getCodigo());
-  }
-  
-  @Test
-  public void dadoUmaStringComUmCodigoNegativoUmNomeEUmaCategoriaDeVendedorRetornaUmVendedor(){
-      Parser<Vendedor> parserVendedor = new ParserVendedor();
-      Vendedor vendedor = parserVendedor.processa("-0021;JOANA ANGELICA;2");
-      
-      assertNull(vendedor);
-  }
-  
-  @Test
-  public void dadoUmaStringComUmCodigoUmNomeEUmaCategoriaIgualA3DeVendedorRetornaUmVendedor(){
-      Parser<Vendedor> parserVendedor = new ParserVendedor();
-      Vendedor vendedor = parserVendedor.processa("0021;JOANA ANGELICA;3");
-      
-      assertNull(vendedor);
-  }
-  
-  @Test
-  public void dadoUmaStringComUmCodigoUmNomeEUmaCategoriaIgualA0DeVendedorRetornaUmVendedor(){
-      Parser<Vendedor> parserVendedor = new ParserVendedor();
-      Vendedor vendedor = parserVendedor.processa("0021;JOANA ANGELICA;0");
-      
-      assertNull(vendedor);
-  }
+    @Before
+    public void setUp(){
+        parserVendedor = new ParserVendedor();
+
+    }
+    
+    @Test
+    public void dadoUmaStringComUmCodigo21UmNomeEUmaCategoria2DeVendedorRetornaUmVendedor() {
+        Vendedor vendedor = parserVendedor.processa("0021;JOANA ANGELICA;2");
+        assertEquals(21, vendedor.getCodigo());
+        assertEquals("JOANA ANGELICA", vendedor.getNome());
+        assertEquals(2, vendedor.getCategoria());
+    }
+    
+   @Test
+    public void dadoUmaStringComUmCodigo20UmNomeEUmaCategoria1DeVendedorRetornaUmVendedor() {
+        Vendedor vendedor = parserVendedor.processa("0020;MANOEL DA SILVA;1");
+        assertEquals(20, vendedor.getCodigo());
+        assertEquals("MANOEL DA SILVA", vendedor.getNome());
+        assertEquals(1, vendedor.getCategoria());
+    }
+    
+    @Test
+    public void dadoUmaStringComUmCodigoNegativoUmNomeEUmaCategoriaDeVendedorRetornaNull() {
+        Vendedor vendedor = parserVendedor.processa("-0021;JOANA ANGELICA;2");
+        assertNull(vendedor);
+    }
+
+    @Test
+    public void dadoUmaStringComUmCodigoUmNomeEUmaCategoriaIgualA3DeVendedorRetornaUmNull() {
+        Vendedor vendedor = parserVendedor.processa("0021;JOANA ANGELICA;3");
+        assertNull(vendedor);
+    }
+
+    @Test
+    public void dadoUmaStringComUmCodigoUmNomeEUmaCategoriaIgualA0DeVendedorRetornaUmNull() {
+        Vendedor vendedor = parserVendedor.processa("0021;JOANA ANGELICA;0");
+        assertNull(vendedor);
+    }
+    
+    @Test
+    public void dadoUmaStringComUmCodigoIgualabcUmNomeEUmaCategoriaIDeVendedorRetornaUmNull() {
+        Vendedor vendedor = parserVendedor.processa("abc;JOANA ANGELICA;1");
+        assertNull(vendedor);
+    }
+    
+    @Test
+    public void dadoUmaStringComUmCodigoUmNomeEUmaCategoriaIgualcDeVendedorRetornaUmNull() {
+        Vendedor vendedor = parserVendedor.processa("0021;JOANA ANGELICA;c");
+        assertNull(vendedor);
+    }
 }
