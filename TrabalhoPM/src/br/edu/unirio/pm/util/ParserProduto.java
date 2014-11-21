a1b13ca3954c322ec8a368069eafbc39280ada06
marcelo.castro@uniriotec.br <marcelo.castro@uniriotec.br@169ede93-cfc7-a32f-6d2c-8c41bd67045f>
2014-11-21 23:41:10 +0000

    Ajuste no ParserProduto e em ParserProdutorTest

--- TrabalhoPM/src/br/edu/unirio/pm/util/ParserProduto.java
+++ TrabalhoPM/src/br/edu/unirio/pm/util/ParserProduto.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.edu.unirio.pm.util;
 
 import br.edu.unirio.pm.model.Produto;
@@ -16,37 +15,50 @@ import org.joda.time.format.DateTimeFormatter;
  * @author MCE
  */
 public class ParserProduto implements Parser<Produto> {
-//TODO REMOVER O RETURN NULL
+
+    private final DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyy");
+    private long codigo;
+    private String nome;
+    private LocalDate inicioVigenciaPreco;
+    private double preco;
+
     @Override
     public Produto processa(String linha) {
-      String[] partes = linha.split(";");
-      DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyy");
-      if (partes.length == 2){
-          try {
-            long codigo = Long.parseLong(partes[0]);
-            String nome = partes[1];
-            if(validaProduto(codigo)){
+        String[] partes = linha.split(";");
+
+        if (partes.length == 2) {
+            try {
+                codigo = Long.parseLong(partes[0]);
+                nome = partes[1];
+                validaCodigo(codigo);
                 return new Produto(codigo, nome);
+            } catch (Exception e) {
+                System.out.println("Erro ao processar linha de Produto" + e.getMessage());
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("Erro ao processar linha de Produto");
-        }
-      } else {
-         try {
-            LocalDate inicioVigenciaPreco = formatter.parseLocalDate(partes[0]);;
-            long codigo = Long.parseLong(partes[1]);
-            double preco = Double.parseDouble(partes[2]);
-            if(validaProduto(codigo)){
+        } else {
+            try {
+                inicioVigenciaPreco = formatter.parseLocalDate(partes[0]);
+                codigo = Long.parseLong(partes[1]);
+                preco = Double.parseDouble(partes[2]);
+                validaCodigo(codigo);
+                validaPreco(preco);
                 return new Produto(codigo, preco, inicioVigenciaPreco);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erro ao processar linha de Produto" + e.getMessage());
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("Erro ao processar linha de Produto");
-        } 
-      }
-      return null;
+        }
+        return null;
     }
 
-    private boolean validaProduto(long codigo) {
-        return codigo > 0;
+    private void validaCodigo(long codigo) {
+        if (codigo < 0) {
+            throw new IllegalArgumentException("Código inválido!");
+        }
+    }
+
+    private void validaPreco(double preco) {
+        if(preco < 0.0){
+            throw new IllegalArgumentException("Código inválido!");
+        }
     }
 }
